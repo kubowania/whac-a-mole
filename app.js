bc027cd5d7e38c28df83eadf4a5baf3913c54d68
Jarvis-3000 <sgaud2001@gmail.com>
2020-10-01 09:21:41 +0530

    modified app.js

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ let score = document.querySelector('#score')
 
 let result = 0
 let currentTime = timeLeft.textContent
+let randomPosition=square[Math.floor(Math.random() * 9)]
 
 function randomSquare() {
-  square.forEach(className => {
-    className.classList.remove('mole')
-  })
-  let randomPosition = square[Math.floor(Math.random() * 9)]
+  //no need to traverse all the squares
+  randomPosition.classList.remove('mole')
+
+  randomPosition = square[Math.floor(Math.random() * 9)]
   randomPosition.classList.add('mole')
 
   //assign the id of the randomPosition to hitPosition for us to use later
